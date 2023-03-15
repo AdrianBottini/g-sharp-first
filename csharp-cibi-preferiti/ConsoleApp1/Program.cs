9ba653993bde308f7fe98dea1df6f6ef9949ea93
AdrianBottini <adrianboh@gmail.com>
2023-03-15 15:01:34 +0100

    ciao

---
+++ csharp-cibi-preferiti/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] favouriteFood = { "Frutta", "Pizza", "Pasta", "Sushi" };
+
+for ( int i = 0; i < favouriteFood.Length; i++)
+{
+    Console.WriteLine( (i) + favouriteFood[i] );
+}
+
+Console.WriteLine("in poolposition c'è:" + favouriteFood[3]);
+
+Console.WriteLine("L'ultimo in classifica è:" + favouriteFood[0]);
+Console.WriteLine("La via di mezzo è:" + favouriteFood[favouriteFood.Length / 2]);
